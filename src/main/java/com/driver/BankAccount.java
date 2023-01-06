12cb81af6b96f29e40b962e5e584ad2543cee315
Sourabh <createownpath111@gmail.com>
2023-01-06 23:02:21 +0530

    codes

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -7,9 +7,9 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
-      this.name = name;
-      this.balance = balance;
-      this.minBalance = minBalance;
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
     }
 
     public String getName() {
@@ -23,11 +23,11 @@ public class BankAccount {
     public double getMinBalance() {
         return minBalance;
     }
-    public String generateAccountNumber(int digits, int sum) throws Exception{
+
+    public String generateAccountNumber(int digits, int sum) throws Exception {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-
         if (sum > 9 * digits) {
             throw new Exception("Account Number can not be generated");
         }
@@ -47,11 +47,12 @@ public class BankAccount {
 
     public void deposit(double amount) {
         //add amount to balance
-      balance = balance + amount;
+        balance=balance +amount;
+
     }
 
     public void withdraw(double amount) throws Exception {
-        // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
+        // Remember to throw "" exception, if the remaining amount would be less than minimum balance
         if(this.balance-amount>=getMinBalance())
         {
             balance=balance-amount;
@@ -59,6 +60,7 @@ public class BankAccount {
         else {
             throw new Exception("Insufficient Balance");
         }
+
     }
 
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,5 +1,5 @@
 package com.driver;
-
+import  java.util.*;
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
@@ -7,15 +7,13 @@ public class CurrentAccount extends BankAccount{
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
         super(name,balance,5000);
         this.tradeLicenseId = tradeLicenseId;
-        if(balance<5000)
+        if(balance<5000){
             throw new Exception("Insufficient Balance");
+        }
     }
 
     public void validateLicenseId() throws Exception {
-        // A trade license Id is said to be valid if no two consecutive characters are same
-        // If the license Id is valid, do nothing
-        // If the characters of the license Id can be rearranged to create any valid license Id
-        // If it is not possible, throw "Valid License can not be generated" Exception
+
         if (!isNumberValid(tradeLicenseId)) {
             String rearrangedId = arrangeString(tradeLicenseId);
             if (rearrangedId == "") {
@@ -93,5 +91,4 @@ public class CurrentAccount extends BankAccount{
     public String getTradeLicenseId() {
         return tradeLicenseId;
     }
-
 }
